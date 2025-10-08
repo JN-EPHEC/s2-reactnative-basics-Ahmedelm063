1cfb7b51c183a7f3b1665872684b01b96cda64b5
Ahmedelm063 <He131896@students.ephec.be>
2025-10-08 12:50:06 +0200

    devoir 2 ahmed

--- app/(tabs)/counter.tsx
+++ app/(tabs)/counter.tsx
@@ -1,37 +1,40 @@
 import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
-  // Initialize state for the counter with an initial value of 0.
-  const [count, setCount] = useState(0);
+// Initialize state for the counter with an initial value of 0.
+const [count, setCount] = useState(0);
 
-  // Function to increment the count.
-  const incrementCount = () => {
-    // Write your logic here to increment the count
-  }
+// Function to increment the count.
+const incrementCount = () => {
+setCount(count + 1);
+};
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Replace this part with your soluce</Text>
-    </SafeAreaView>
-  );
+return (
+<SafeAreaView style={styles.container}>
+<Text style={styles.title}><View style={styles.container}>
+<Text style={styles.countText}>Count: {count}</Text>
+<Button title="Press me" onPress={() => setCount(count + 1)} />
+</View></Text>
+</SafeAreaView>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f0f0f0",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  countText: {
-    fontSize: 48,
-    fontWeight: "600",
-    marginVertical: 20,
-  },
+container: {
+flex: 1,
+backgroundColor: "#f0f0f0",
+alignItems: "center",
+justifyContent: "center",
+},
+title: {
+fontSize: 24,
+fontWeight: "bold",
+marginBottom: 20,
+},
+countText: {
+fontSize: 48,
+fontWeight: "600",
+marginVertical: 20,
+},
 });
